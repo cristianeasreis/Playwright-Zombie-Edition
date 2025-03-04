18c2e4c232f5c9e00e271a5ebf9266ada93fbc58
Cristiane Araujo Souza Dos Reis <cristianeasreis@hotmail.com>
2025-03-04 02:51:36 -0300

    iniciando os testes em movies

--- src/tests/e2e/movies.spec.js
+++ src/tests/e2e/movies.spec.js
@@ -1,6 +1,23 @@
-const {test} = require('@playwright/test');
+const { test } = require('@playwright/test');
+
+const { LoginPage } = require('../../tests/pages/LoginPage.js');
+const { MoviesPage } = require('../../tests/pages/MoviesPage.js');
+const { Toast } = require('../../tests/pages/Components.js');
+
+let loginPage;
+let moviesPage;
+let toast;
+
+test.beforeEach(({ page }) => {
+    loginPage = new LoginPage(page);
+    moviesPage = new MoviesPage(page);
+    toast = new Toast(page);
+});
 
 test('deve poder cadastrar um novo filme ', async ({ page }) => {
- 
-    
+
+    await loginPage.Visit();
+    await loginPage.submit('admin@zombieplus.com', 'pwd123');
+    await moviesPage.isLoggedIn();
+
 });
