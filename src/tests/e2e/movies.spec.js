ddc6792d39e9bab500e1df6ffb67613baa6d6926
Cristiane Araujo Souza Dos Reis <cristianeasreis@hotmail.com>
2025-03-08 04:17:34 -0300

    ajuste na validação de boas vidas

--- src/tests/e2e/movies.spec.js
+++ src/tests/e2e/movies.spec.js
@@ -6,14 +6,14 @@ const { executeSQL } = require('../../tests/support/database.js');
 test('deve poder cadastrar um novo filme ', async ({ page }) => {
     const movie = data.create
     await executeSQL(`DELETE FROM movies WHERE title = '${movie.title}'`);  
-    await page.login.do('admin@zombieplus.com', 'pwd123');  
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin');  
     await page.movies.create(movie.title, movie.overview, movie.company, movie.release_year);
     await page.toast.containText('Cadastro realizado com sucesso!');
 
 });
 
 test('não deve cadastrar quando os campos obrigatórios não são preenchidos', async ({ page }) => {   
-    await page.login.do('admin@zombieplus.com', 'pwd123');   
+    await page.login.do('admin@zombieplus.com', 'pwd123' , 'Admin');   
     await page.movies.goForm();
     await page.movies.submit();
     await page.movies.alertHaveText([
