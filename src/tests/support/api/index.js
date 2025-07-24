53b17e76f324e70fcb2d2b3df8b8a0a1d0c2064f
Cristiane Araujo Souza Dos Reis <c.souza.dos.reis@avanade.com>
2025-07-24 20:54:04 -0300

    subindo aula evitando massa hard coded

--- src/tests/support/api/index.js
+++ src/tests/support/api/index.js
@@ -19,20 +19,40 @@ export class Api {
         const body = JSON.parse(await response.text());
         this.token = 'Bearer ' + body.token;
 
-
     }
-    async postMovie(movie) {
+
+    async getCompanyIdByName(companyName) {
         await this.setToken();
-        
-        const response = await this.request.post('http://localhost:3333/movies', {
+
+        const response = await this.request.get('http://localhost:3333/companies', {
             headers: {
                 Authorization: this.token,                
+            },
+            params: {
+                name: companyName
+            }
+
+        });
+
+        expect(response.ok()).toBeTruthy();
+
+        const body = JSON.parse(await response.text());
+        return body.data[0].id;
+    }
+
+    async postMovie(movie) {
+        const companyId = await this.getCompanyIdByName(movie.company)
+        await this.setToken();
+
+        const response = await this.request.post('http://localhost:3333/movies', {
+            headers: {
+                Authorization: this.token,
                 Accept: 'application/json, text/plain, */*'
             },
-            multipart:{
+            multipart: {
                 title: movie.title,
-                overview: movie.overView, 
-                company_id: '71c4a30b-7c36-4c2a-aa2b-4468e00c64fe',
+                overview: movie.overView,
+                company_id: companyId,
                 release_year: movie.release_year,
                 featured: movie.featured,
             }
